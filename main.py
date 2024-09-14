84ab6518b4ee798fd39f505b811e4174458247c4
Sanjeeb Dey <39940629+deysanjeeb@users.noreply.github.com>
2024-09-14 22:06:52 +0000

    replit version

--- main.py
+++ main.py
@@ -6,10 +6,13 @@ from dotenv import load_dotenv
 from Prompts.NegotiatorAgent import PROMPT_TEMPLATE
 
 app = FastAPI()
+# local run
+# load_dotenv()
+# client = Together(api_key=os.environ.get("TOGETHER_API_KEY"))
 
-load_dotenv()
-
-client = Together(api_key=os.environ.get("TOGETHER_API_KEY"))
+# replit run
+client = Together(api_key=os.getenv("TOGETHER_API_KEY"))
+print(os.getenv("TOGETHER_API_KEY"))
 
 class ChatRequest(BaseModel):
     message: str
@@ -30,9 +33,21 @@ async def chat_endpoint(request: ChatRequest):
             max_tokens=4096,
             temperature=0.3
         )
+        print(completion.choices[0].message.content)
         return ChatResponse(response=completion.choices[0].message.content)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.get("/item")
+async def iten_endpoint():
+    return {"name": "Chair", "description": "Comfy Blue chair", "price": 10, "URL": "https://example.com"}
+
+
+@app.post("/item")
+async def iten_endpoint():
+    return {"name": "Chair", "description": "Comfy Blue chair", "price": 10, "URL": "https://example.com"}
+
 
 if __name__ == "__main__":
     import uvicorn
