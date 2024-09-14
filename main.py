5288c038969508ff057441d9a2d25ddc5a2d8d15
jannalulu <109004049+jannalulu@users.noreply.github.com>
2024-09-14 16:53:19 -0400

    add chat history and change temperature

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from together import Together
 from dotenv import load_dotenv
+from Prompts.NegotiatorAgent import PROMPT_TEMPLATE
 
 app = FastAPI()
 
@@ -12,6 +13,7 @@ client = Together(api_key=os.environ.get("TOGETHER_API_KEY"))
 
 class ChatRequest(BaseModel):
     message: str
+    chat_history: list = []
 
 class ChatResponse(BaseModel):
     response: str
@@ -19,9 +21,14 @@ class ChatResponse(BaseModel):
 @app.post("/chat", response_model=ChatResponse)
 async def chat_endpoint(request: ChatRequest):
     try:
+        messages = request.chat_history + [{"role": "user", 
+                                            "content": PROMPT_TEMPLATE.format(request=request.message)}]
+        
         completion = client.chat.completions.create(
             model="meta-llama/Meta-Llama-3.1-70B-Instruct-Turbo",
-            messages=[{"role": "user", "content": request.message}]
+            messages=messages,
+            max_tokens=4096,
+            temperature=0.3
         )
         return ChatResponse(response=completion.choices[0].message.content)
     except Exception as e:
